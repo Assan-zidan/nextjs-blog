9d67d37874f8c4e9ae652560954e49b55a0ab467
Assan-zidan <nsangouassanzidan@gmail.com>
2022-10-24 16:21:36 +0200

    add coe

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,9 @@
 // import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+import Date from '../components/date';
 
-// export default function Home() {
-//   return (
-//     <Layout home>
-//       <Head>
-//         <title>{siteTitle}</title>
-//       </Head>
-//       <section className={utilStyles.headingMd}>
-//         <p>[Your Self Introduction]</p>
-//         <p>
-//           (This is a sample website - you’ll be building a site like this on{' '}
-//           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-//         </p>
-//       </section>
-//     </Layout>
-//   );
-// }
 
 import { getSortedPostsData } from '../lib/posts';
 
@@ -34,7 +20,9 @@ export default function Home({ allPostsData }) {
   return (
     <Layout home>
       {/* Keep the existing code here */}
-
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
 
 
       {/* Add this <section> tag below the existing <section> tag */}
@@ -43,11 +31,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
